09a7f364666a8d7eabed9aa7d47581137b9b6943
Caner Korkmaz <info@canerkorkmaz.com>
2018-12-10 14:46:03 +0300

    Added gradle and project structure

---
+++ src/main/java/group1/hw3/Application.java
@@ -0,0 +1,8 @@
+package group1.hw3;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Homework3");
+    }
+}
---
+++ src/test/java/group1/hw3/tests/ApplicationTests.java
@@ -0,0 +1,11 @@
+package group1.hw3.tests;
+
+import group1.hw3.Application;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+    @Test
+    public void contextLoads() {
+        Application.main(new String[]{});
+    }
+}
